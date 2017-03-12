fd704f633d97f5d91da8449bdef0257a1d0f8c51
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-13 02:55:11 +0900

    rename migration

--- database/migrations/2017_03_11_075348_create_tasks_table.php
+++ database/migrations/2017_03_11_075348_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasks extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateTasks extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->index();
+            $table->integer('user_id')->references('id')->on('users')->unsigned()->index();
             $table->string('title');
             $table->boolean('is_done')->default(false);
 
