ed2595c1e6cb11e11f84fb0040634c33015b00fb
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-26 22:09:26 +0900

    add logout route

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ Route::group(['middleware' => 'api'], function () {
     Route::post('authenticate',  'AuthenticateController@authenticate');
     Route::get('users',  'AuthenticateController@getUsers');
 
+    Route::get('logout',  'AuthenticateController@logout')->middleware('jwt.refresh');
+
     Route::group(['middleware' => 'jwt.auth'], function () {
         Route::resource('tasks',  'TaskController');
         Route::get('me',  'AuthenticateController@getCurrentUser');
